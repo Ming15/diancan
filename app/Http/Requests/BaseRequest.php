d656cb992b9263b4fd0704ff0a2570793780d79b
siiiyaa <2863758304@qq.com>
2021-10-08 22:17:53 +0800

    修改表单验证生成模版和新建公用Request文件，统一表单验证错误返回格式

---
+++ app/Http/Requests/BaseRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Traits\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BaseRequest extends FormRequest
+{
+    use ApiResponse;
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        $error= $validator->errors()->all();
+        throw new HttpResponseException($this->setHttpCode(400)->error([], $error[0]));
+
+    }
+}
