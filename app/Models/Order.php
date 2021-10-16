f926b27e993f0cd1b46c2c56903450a32d60ea63
siiiyaa <2863758304@qq.com>
2021-10-16 11:20:53 +0800

    订单日期格式优化

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Claims\DatetimeTrait;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory, DatetimeTrait;
 
     protected $table = 'orders';
 
@@ -87,5 +89,10 @@ class Order extends Model
     public function getStatusTextAttribute()
     {
         return self::$status[$this->status];
+    }
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
     }
 }
